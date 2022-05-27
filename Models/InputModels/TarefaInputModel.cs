590b353bb665b2d709d40bd9c75333f9e19f1c65
James Noleto <muricijames@gmail.com>
2022-05-26 21:47:53 -0300

    feat: input de tarefa

---
+++ Models/InputModels/TarefaInputModel.cs
@@ -0,0 +1,11 @@
+namespace MongoDb.Models.InputModels
+{
+    public class TarefaInputModel
+    {
+        public string Nome { get; set; }
+
+        public string Detalhes { get; set; }
+
+        public bool? Concluido { get; set; }
+    }
+}
