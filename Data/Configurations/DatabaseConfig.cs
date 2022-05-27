2d856bfb294f22b0e7f46bb8002c5dbac0312364
James Noleto <muricijames@gmail.com>
2022-05-26 21:46:37 -0300

    feat: classe de configuração

---
+++ Data/Configurations/DatabaseConfig.cs
@@ -0,0 +1,8 @@
+namespace MongoDb.Data.Configurations
+{
+    public class DatabaseConfig : IDatabaseConfig
+    {
+        public string DatabaseName { get;  set; }
+        public string ConnectionString { get; set; }
+    }
+}
