b5a81e535d880920f6123e4284d545b3832d0501
James Noleto <muricijames@gmail.com>
2022-05-26 21:47:11 -0300

    feat: interface do repositório

---
+++ Data/Repositories/ITarefaRepository.cs
@@ -0,0 +1,17 @@
+using MongoDb.Models;
+
+namespace MongoDb.Data.Repositories
+{
+    public interface ITarefasRepository
+    {
+        void Adicionar(Tarefa tarefa);
+
+        void Atualizar(string id, Tarefa tarefaAtualizada);
+
+        IEnumerable<Tarefa> Buscar();
+
+        Tarefa Buscar(string id);
+
+        void Remover(string id);
+    }
+}
