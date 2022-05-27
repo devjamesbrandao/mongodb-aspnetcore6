f7835813d7c823b0734327efb5f9bb8b069c562f
James Noleto <muricijames@gmail.com>
2022-05-26 21:46:52 -0300

    feat: interface de configuração

---
+++ Data/Configurations/IDatabaseConfig.cs
@@ -0,0 +1,9 @@
+namespace MongoDb.Data.Configurations
+{
+    public interface IDatabaseConfig
+    {
+        string DatabaseName { get; set; }
+
+        string ConnectionString { get; set; }
+    }
+}
