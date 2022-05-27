e1980e5afad986b19d3b5593725b369c1f93b6b3
James Noleto <muricijames@gmail.com>
2022-05-26 21:46:09 -0300

    feat: injeção de dependências

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Options;
+using MongoDb.Data.Configurations;
+using MongoDb.Data.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<DatabaseConfig>(builder.Configuration.GetSection(nameof(DatabaseConfig)));
+
+builder.Services.AddSingleton<IDatabaseConfig>(sp => sp.GetRequiredService<IOptions<DatabaseConfig>>().Value);
+
+builder.Services.AddSingleton<ITarefasRepository, TarefasRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
